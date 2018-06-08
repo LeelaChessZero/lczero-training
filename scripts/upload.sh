d7126ff8dc57b78ce30994ce303b9f8fa3943295
Error323 <folkerthuizinga@gmail.com>
2018-06-08 18:27:52 +0200

    indentation on upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,49 +4,51 @@ set -e
 
 function usage()
 {
-    echo "Uploads a network with NxM prefix, where N=filters and M=blocks"
-    echo ""
-    echo "./upload.sh"
-    echo "  -h --help"
-    echo "  -u --upload   The upload url"
-    echo "  -d --netdir   The directory where new networks arrive"
-    echo "  -f --filters  Number of filters"
-    echo "  -b --blocks   Number of blocks"
-    echo ""
-    echo "Example: ./upload.sh -d=/tmp -u=http://upload.me -f=64 -b=6"
-    echo ""
+  echo "Uploads a network with NxM prefix, where N=filters and M=blocks"
+  echo ""
+  echo "./upload.sh"
+  echo "  -h --help"
+  echo "  -u --upload   The upload url"
+  echo "  -d --netdir   The directory where new networks arrive"
+  echo "  -f --filters  Number of filters"
+  echo "  -b --blocks   Number of blocks"
+  echo ""
+  echo "Example: ./upload.sh -d=/tmp -u=http://upload.me -f=64 -b=6"
+  echo ""
 }
 
 while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
-    case $PARAM in
-        -h | --help)
-            usage
-            exit
-            ;;
-        -u | --upload)
-            UPLOADURL=$VALUE
-            ;;
-        -d | --netdir)
-            NETDIR=$VALUE
-            ;;
-        -f | --filters)
-            FILTERS=$VALUE
-            ;;
-        -b | --blocks)
-            BLOCKS=$VALUE
-            ;;
-        *)
-            echo "ERROR: unknown parameter \"$PARAM\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
+  PARAM=`echo $1 | awk -F= '{print $1}'`
+  VALUE=`echo $1 | awk -F= '{print $2}'`
+  case $PARAM in
+    -h | --help)
+        usage
+        exit
+        ;;
+    -u | --upload)
+        UPLOADURL=$VALUE
+        ;;
+    -d | --netdir)
+        NETDIR=$VALUE
+        ;;
+    -f | --filters)
+        FILTERS=$VALUE
+        ;;
+    -b | --blocks)
+        BLOCKS=$VALUE
+        ;;
+    *)
+        echo "ERROR: unknown parameter \"$PARAM\""
+        usage
+        exit 1
+        ;;
+  esac
+  shift
 done
 
 netarch="${FILTERS}x${BLOCKS}"
+
+echo "start upload monitor for $netarch*.gz"
 
 inotifywait -q -m -e close_write $NETDIR | 
 while read -r dir events file
