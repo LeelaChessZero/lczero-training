5aceafd282905481a8e2109a3abf3fc83172c14b
F. Huizinga <folkerthuizinga@gmail.com>
2018-06-09 09:16:19 +0200

    simplify split.sh and respect windowsize (#16)
    * simplify split.sh and respect windowsize
    
    * add mbuffer to ensure data integrity
    
    * fix usage info
    
    * minor tweaks
    
    * process existing files in directory
    
    * bugfix
    
    * fix pr review
    
    * observe inotify moved_to and close_write
    
    * enable debugging info
    
    * bash fix
    
    * disable ls sorting
    
    * enable sorting again...

--- scripts/split.sh
+++ scripts/split.sh
@@ -1,69 +1,116 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-NETDIR="/home/folkert/data/run1"
-TSTDIR="/home/folkert/data/work/test"
-TRNDIR="/home/folkert/data/work/train"
+function usage()
+{
+  echo "Watches a directory and copies data to train/test set"
+  echo ""
+  echo "./split.sh"
+  echo "  -h --help"
+  echo "  -i --input   The directory where chunks arrive"
+  echo "  -o --output  The output directory"
+  echo "  -n --window  window size of test + train"
+  echo "  -t --train   The training percentage in {1,...,100}"
+  echo ""
+  echo "Example: ./split.sh -i=/tmp -o=/out -n=2000 -t=95"
+  echo ""
+}
 
-cd /home/folkert/data/work
-echo "monitor start"
+while [ "$1" != "" ]
+do
+  PARAM=`echo $1 | awk -F= '{print $1}'`
+  VALUE=`echo $1 | awk -F= '{print $2}'`
+  case $PARAM in
+    -h | --help)
+      usage
+      exit
+      ;;
+    -i | --input)
+      INPUTDIR=$VALUE
+      ;;
+    -o | --output)
+      TESTDIR="$VALUE/test"
+      TRAINDIR="$VALUE/train"
+      ;;
+    -n | --window)
+      WINSIZE=$VALUE
+      ;;
+    -t | --train)
+      TRAINPCT=$VALUE
+      ;;
+    *)
+      echo "ERROR: unknown parameter \"$PARAM\""
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+
+# clear test and train split dirs
+rm -rf $TESTDIR $TRAINDIR
+mkdir -vp $TESTDIR $TRAINDIR
 
 n=0
-cn=100
-lval=6553
+let overhead="$WINSIZE / 10"
+let max="$WINSIZE + $overhead"
+overhead_train=$(echo "scale=1;($TRAINPCT / 100) * $overhead" | bc | cut -d'.' -f1)
+overhead_test=$(echo "scale=1;(1 - $TRAINPCT / 100) * $overhead" | bc | cut -d'.' -f1)
 
-ntst=0
-ntrn=0
-ntsttot=0
-ntrntot=0
+echo ""
+echo "start splitter"
+echo "  max chunks: $max"
+echo "  max test:   $overhead_test"
+echo "  max train:  $overhead_train"
+echo ""
 
 
-inotifywait -m $NETDIR -e create -e moved_to | mbuffer -m 10M |
-    while read path action file; do
+process() {
+  local dir=$1
+  local file=$2
 
-        if [ "$action" = "MOVED_TO" ]
-        then
-                let "n++"
+  if [[ $file = training.*.gz ]]
+  then
+    let "n++"
 
-                #echo "The file '$file' appeared in directory '$path' via '$action'"
-                sha=$(sha256sum $NETDIR/$file | awk '{print $1}' | cut -c1-4)
-                val=$((16#$sha))
+    id=$(echo $file | cut -d'.' -f 2)
+    let x="$id % 100 + 1"
 
-                fn=$NETDIR/$file
+    if [ $x -gt $TRAINPCT ]
+    then
+      target=$TESTDIR/$file
+      echo -n "T"
+    else
+      target=$TRAINDIR/$file
+      echo -n "*"
+    fi
 
-                if [ $val -lt $lval ]
-                then
-                        echo -n "*"
-                        let "ntst++"
-                        let "ntsttot++"
-                        target=$TSTDIR/$file
-                else
-                        echo -n "T"
-                        let "ntrn++"
-                        let "ntrntot++"
-                        target=$TRNDIR/$file
-                fi
+    cp -a $dir/$file $target
 
-                if [ ! -f $target ]
-                then
-                        cp --preserve=timestamps $fn $target
-                fi
-        else
-                if [ "$action" = "CREATE" ]
-                then
-                        ec=$(echo " ")
-                else
-                        echo "The file '$file' appeared in directory '$path' via '$action'"
-                fi
-        fi
+    if [ $n -gt $max ]
+    then
+      ls -rt $TESTDIR | head -n $overhead_test | xargs -I{} rm -f $TESTDIR/{}
+      ls -rt $TRAINDIR | head -n $overhead_train | xargs -I{} rm -f $TRAINDIR/{}
+      let "n -= $overhead"
+    fi
+  fi
+}
 
-        if [ $n -eq $cn ]
-        then
-                echo " - $(date) - $ntst $ntrn $ntsttot $ntrntot - $file"
 
-                n=0
-                ntst=0
-                ntrn=0
-        fi
+echo -n "processing data in '$INPUTDIR'..."
+for f in $(ls -1rt $INPUTDIR | tail -n $WINSIZE)
+do
+  file=$(basename $f)
+  process $INPUTDIR $file
+done
+echo "[done]"
 
-    done
-
+echo "monitoring '$INPUTDIR'"
+inotifywait -q -m -e moved_to -e close_write $INPUTDIR | mbuffer -m 10M |
+  while read dir event file
+  do
+    if [[ $event = MOVED_TO ]] || [[ $event = CLOSE_WRITE ]]
+    then
+      process $INPUTDIR $file
+    fi
+  done
