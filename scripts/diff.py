fda68ee249ec4e724f63e5fa362bb411dab2fe10
Error323 <folkerthuizinga@gmail.com>
2018-06-10 19:14:10 +0200

    optimize splitting

--- scripts/diff.py
+++ scripts/diff.py
@@ -8,7 +8,7 @@ import argparse
 def get_latest_chunk_ids(dirs):
     chunks = []
     for d in dirs:
-        for f in glob.glob(d + "/training.*.gz"):
+        for f in glob.glob(os.path.join(d, "training.*.gz")):
             chunks.append(int(os.path.basename(f).split('.')[-2]))
     chunks.sort(reverse=True)
     return chunks
@@ -16,7 +16,7 @@ def get_latest_chunk_ids(dirs):
 
 def main(argv):
     a = get_latest_chunk_ids([argv.input])
-    n = min(argv.winsize, len(a))
+    n = min(argv.wsize, len(a))
     b = get_latest_chunk_ids(argv.dirs)
     diff = set(a[:n]) - set(b)
     for i in diff:
@@ -26,9 +26,9 @@ def main(argv):
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=\
     'Print diffset of input dir and output dirs.')
-    argparser.add_argument('--input', type=str,
+    argparser.add_argument('-i', '--input', type=str,
             help='input directory')
-    argparser.add_argument('--winsize', type=int,
+    argparser.add_argument('-w', '--wsize', type=int,
             help='window size')
     argparser.add_argument('dirs', nargs='+',
             help='output directories')
