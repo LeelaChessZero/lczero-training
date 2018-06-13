af2fb9a8852cfd3c04b37bf45d3846d64b2d0ca4
Error323 <folkerthuizinga@gmail.com>
2018-06-13 20:05:47 +0200

    code cleanup

--- scripts/diff.py
+++ scripts/diff.py
@@ -5,19 +5,19 @@ import os
 import argparse
 
 
-def get_latest_chunk_ids(dirs):
-    chunks = []
+def get_sorted_chunk_ids(dirs):
+    ids = []
     for d in dirs:
         for f in glob.glob(os.path.join(d, "training.*.gz")):
-            chunks.append(int(os.path.basename(f).split('.')[-2]))
-    chunks.sort(reverse=True)
-    return chunks
+            ids.append(int(os.path.basename(f).split('.')[-2]))
+    ids.sort(reverse=True)
+    return ids
 
 
 def main(argv):
-    a = get_latest_chunk_ids([argv.input])
+    a = get_sorted_chunk_ids([argv.input])
+    b = get_sorted_chunk_ids(argv.dirs)
     n = min(argv.wsize, len(a))
-    b = get_latest_chunk_ids(argv.dirs)
     diff = set(a[:n]) - set(b)
     for i in sorted(diff):
         print('training.{}.gz'.format(i))
@@ -25,7 +25,7 @@ def main(argv):
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=\
-    'Print diffset of input dir and output dirs.')
+            'Print diffset of input dir and output dirs.')
     argparser.add_argument('-i', '--input', type=str,
             help='input directory')
     argparser.add_argument('-w', '--wsize', type=int,
--- scripts/split.sh
+++ scripts/split.sh
@@ -133,8 +133,5 @@ echo -e "\nmonitoring '$INPUTDIR'"
 inotifywait -q -m -e moved_to -e close_write $INPUTDIR | mbuffer -m 10M |
   while read dir event file
   do
-    if [[ $event = *MOVED_TO* ]] || [[ $event = *CLOSE_WRITE* ]]
-    then
-      process $INPUTDIR $file
-    fi
+    process $INPUTDIR $file
   done
