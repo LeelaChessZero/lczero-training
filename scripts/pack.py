628ca552fca1b4b496094650077ae523eda17c8c
F. Huizinga <folkerthuizinga@gmail.com>
2018-07-06 20:01:03 +0200

    Transposedcompression (#26)
    * pack chunks in bz2
    
    * add unpack.py and fix pack.py
    
    * remove debug info
    
    * optionally remove source files
    
    * bugfix in get_sorted_chunk_ids
    
    * do not reverse ids anymore
    
    * x = unpack(pack(x)) tested inverse function
    
    * simplify packing algo
    
    * minor tweaks

---
+++ scripts/pack.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import argparse
+import gzip
+import bz2
+import struct
+import numpy as np
+from multiprocessing import Pool
+
+
+RECORD_SIZE = 8276
+
+
+def get_uncompressed_size(filename):
+    with open(filename, 'rb') as f:
+        f.seek(-4, 2)
+        return struct.unpack('I', f.read(4))[0]
+
+
+def get_sorted_chunk_ids(dirs):
+    ids = []
+    for d in dirs:
+        for f in glob.glob(os.path.join(d, "training.*.gz")):
+            ids.append(int(os.path.basename(f).split('.')[-2]))
+    ids.sort()
+    return ids
+
+
+def pack(ids):
+    plies = []
+    fout_name = os.path.join(argv.output, '{}-{}.bz2'.format(ids[0], ids[-1]))
+    with bz2.open(fout_name, 'xb') as fout:
+        for tid in ids:
+            fin_name = os.path.join(argv.input, 'training.{}.gz'.format(tid))
+            plies.append(get_uncompressed_size(fin_name) // RECORD_SIZE)
+            with gzip.open(fin_name, 'rb') as fin:
+                fout.write(fin.read())
+            if argv.remove:
+                os.remove(fin_name)
+
+        plylist = np.array(plies, dtype=np.int16)
+        size = struct.pack('I', len(plylist)*2)
+        fout.write(plylist.tobytes())
+        fout.write(size)
+
+    print("Written '{}' {} records".format(fout_name, np.sum(plies)))
+
+
+def main():
+    if not os.path.exists(argv.output):
+        os.makedirs(argv.output)
+        print("Created directory '{}'".format(argv.output))
+
+    ids = get_sorted_chunk_ids([argv.input])
+    n = len(ids) // argv.number
+    m = argv.number
+    print("Processing {} ids, {} - {} ({}x{})".format(len(ids), ids[0], ids[-1], n, m))
+    packs = [ids[i*m:i*m+m] for i in range(n)]
+
+    # add remaining ids to last pack
+    packs[-1] += ids[n*m+m:]
+
+    with Pool() as pool:
+        pool.map(pack, packs)
+                
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=\
+            'Repack training.*.gz files in batches of bz2 format.')
+    argparser.add_argument('-i', '--input', type=str,
+            help='input directory')
+    argparser.add_argument('-o', '--output', type=str,
+            help='output directory')
+    argparser.add_argument('-r', '--remove', action='store_true',
+            help='remove input files while processing')
+    argparser.add_argument('-n', '--number', type=int, default=1000,
+            help='number of games to repack per bz2 package')
+    argv = argparser.parse_args()
+
+    main()
---
+++ scripts/unpack.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import gzip
+import bz2
+import numpy as np
+import pickle
+import struct
+from pack import RECORD_SIZE
+
+
+def unpack(filepath):
+    front, back = os.path.basename(filepath).split('-')
+    back = back.split('.')[0]
+    first = int(front)
+    last = int(back)
+    num_chunks = last - first + 1
+
+    buf = bz2.BZ2File(filepath, 'rb').read()
+    size = struct.unpack('I', buf[-4:])[0]
+    plylist = np.frombuffer(buf[-4-size:-4], dtype=np.int16)
+    data = np.frombuffer(buf[:-4-size], dtype=np.int8).reshape(-1, RECORD_SIZE)
+    assert(num_chunks == len(plylist))
+
+    begin = 0
+    for i, plies in enumerate(plylist):
+        end = begin + plies
+        filename = os.path.join(argv.output, "training.{}.gz".format(i + first))
+
+        with gzip.open(filename, 'wb') as f:
+            for row in data[begin:end]:
+                f.write(row)
+
+        begin = end
+
+    print("Written {} chunks".format(num_chunks))
+
+
+def main():
+    if not os.path.exists(argv.output):
+        os.makedirs(argv.output)
+        print("Created directory '{}'".format(argv.output))
+
+    unpack(argv.input)
+                
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=\
+            'Unpack *-*.bz2 file into gz chunks.')
+    argparser.add_argument('-i', '--input', type=str,
+            help='input file')
+    argparser.add_argument('-o', '--output', type=str,
+            help='output directory')
+    argv = argparser.parse_args()
+
+    main()
