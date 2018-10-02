Filter out moving mean and variance at correct place

tf/tfprocess.py
@@ -359,12 +359,12 @@ class TFProcess:
                   before in zip(after_weights, before_weights)]
         delta_norms = [np.linalg.norm(d.ravel()) for d in deltas]
         weight_norms = [np.linalg.norm(w.ravel()) for w in before_weights]
-        ratios = [d / w for d, w in zip(delta_norms, weight_norms)]
+        ratios = [(tensor.name, d / w) for d, w, tensor in zip(delta_norms, weight_norms, self.weights) if not 'moving' in tensor.name]
         all_summaries = [
             tf.Summary.Value(tag='update_ratios/' +
                              tensor.name, simple_value=ratio)
-            for tensor, ratio in zip(self.weights, ratios) if not 'moving' in tensor.name]
-        ratios = np.log10([r for r in ratios if 0 < r < np.inf])
+            for tensor, ratio in ratios]
+        ratios = np.log10([r for (_, r) in ratios if 0 < r < np.inf])
         all_summaries.append(self.log_histogram('update_ratios_log10', ratios))
         return tf.Summary(value=all_summaries)
 