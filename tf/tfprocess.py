Fix wrong shape of illegal filler

tf/tfprocess.py
@@ -102,9 +102,10 @@ class TFProcess:
         # Calculate loss on policy head
         if self.cfg['training'].get('mask_legal_moves'):
             # extract mask for legal moves from target policy
-            move_is_legal = self.y_ >= 0
+            move_is_legal = tf.greater_equal(self.y_, 0)
             # replace logits of illegal moves with large negative value (so that it doesn't affect policy of legal moves) without gradient
-            self.y_conv = tf.where(move_is_legal, self.y_conv, -1.0e10)
+            illegal_filler = tf.zeros_like(self.y_conv) - 1.0e-10
+            self.y_conv = tf.where(move_is_legal, self.y_conv, illegal_filler)
         # y_ still has -1 on illegal moves, flush them to 0
         self.y_ = tf.nn.relu(self.y_)
         cross_entropy = \