5df3102746d5004e3692e2e01702e386926d3fe3
Tilps <Tilps@users.noreply.github.com>
2019-05-25 23:27:10 +1000

    Add some useful scripts (#77)
    fixorder.py is useful when starting training for the first time and rescoring has messed with the timestamps on the files so they don't match the numerical order anymore.
    initsplit.py is a super fast alternative to split.sh that works as a once off when setting up training for the first time.
    rescore.sh runs rescorer in a loop - should take more parameters...
    stage.sh watches file system events and moves files to a different directory (on the same file system) so that rescorer never tries to rescore a partially written file.

---
+++ scripts/fixorder.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import argparse
+
+
+def get_sorted_chunk_ids(dirs):
+    ids = []
+    for d in dirs:
+        for f in glob.glob(os.path.join(d, "training.*.gz")):
+            ids.append(int(os.path.basename(f).split('.')[-2]))
+    ids.sort()
+    return ids
+
+
+def main(argv):
+    a = get_sorted_chunk_ids([argv.input])
+    for i in a:
+        os.utime(os.path.join(argv.input, "training.{}.gz".format(i)), None)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=\
+            'Change modification time on training files to match their numeric order.')
+    argparser.add_argument('-i', '--input', type=str,
+            help='input directory')
+
+    main(argparser.parse_args())
---
+++ scripts/initsplit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import argparse
+
+
+def get_sorted_chunk_ids(dirs):
+    ids = []
+    for d in dirs:
+        for f in glob.glob(os.path.join(d, "training.*.gz")):
+            ids.append(int(os.path.basename(f).split('.')[-2]))
+    ids.sort(reverse=True)
+    return ids
+
+
+def main(argv):
+    a = get_sorted_chunk_ids([argv.input])
+    n = min(argv.wsize, len(a))
+    for i in sorted(a[:n]):
+        if i % 100 >= 90:
+            os.link(os.path.join(argv.input, "training.{}.gz".format(i)), os.path.join(argv.output, "test/training.{}.gz".format(i)))
+        else:
+            os.link(os.path.join(argv.input, "training.{}.gz".format(i)), os.path.join(argv.output, "train/training.{}.gz".format(i)))
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description=\
+            'Link input to test/train subdirectories of output in 10:90 ratio.')
+    argparser.add_argument('-i', '--input', type=str,
+            help='input directory')
+    argparser.add_argument('-w', '--wsize', type=int,
+            help='window size - should be padded a bit to ensure both sides of split exceed fraction of target')
+    argparser.add_argument('-o', '--output', type=str,
+            help='output directory')
+
+    main(argparser.parse_args())
