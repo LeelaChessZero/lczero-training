Add policy accuracy function that corrects for masking.

tf/tfprocess.py
@@ -162,6 +162,19 @@ class TFProcess:
                                                         logits=output)
             return tf.reduce_mean(input_tensor=policy_cross_entropy)
         self.policy_loss_fn = policy_loss
+        def policy_accuracy(target, output):
+            output = tf.cast(output, tf.float32)
+            # Calculate loss on policy head
+            if self.cfg['training'].get('mask_legal_moves'):
+                # extract mask for legal moves from target policy
+                move_is_legal = tf.greater_equal(target, 0)
+                # replace logits of illegal moves with large negative value (so that it doesn't affect policy of legal moves) without gradient
+                illegal_filler = tf.zeros_like(output) - 1.0e10
+                output = tf.where(move_is_legal, output, illegal_filler)
+            # y_ still has -1 on illegal moves, flush them to 0
+            target = tf.nn.relu(target)
+            return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(input=target, axis=1), tf.argmax(input=output, axis=1)), tf.float32))
+        self.policy_accuracy_fn = policy_accuracy
 
 
         q_ratio = self.cfg['training'].get('q_ratio', 0)
@@ -505,7 +518,7 @@ class TFProcess:
     def calculate_test_summaries_inner_loop(self, x, y, z, q):
         policy, value = self.model(x, training=False)
         policy_loss = self.policy_loss_fn(y, policy)
-        policy_accuracy = self.accuracy_fn(y, policy)
+        policy_accuracy = self.policy_accuracy_fn(y, policy)
         if self.wdl:
             value_loss = self.value_loss_fn(self.qMix(z, q), value)
             mse_loss = self.mse_loss_fn(self.qMix(z, q), value)