Don't output the wrong shape so we don't have to immediately reshape it.

tf/chunkparsefunc.py
@@ -28,7 +28,7 @@ def parse_function(planes, probs, winner, q, plies_left):
     q = tf.io.decode_raw(q, tf.float32)
     plies_left = tf.io.decode_raw(plies_left, tf.float32)
 
-    planes = tf.reshape(planes, (-1, 112, 8 * 8))
+    planes = tf.reshape(planes, (-1, 112, 8, 8))
     probs = tf.reshape(probs, (-1, 1858))
     winner = tf.reshape(winner, (-1, 3))
     q = tf.reshape(q, (-1, 3))

tf/tfprocess.py
@@ -243,9 +243,8 @@ class TFProcess:
 
     def init_net(self):
         self.l2reg = tf.keras.regularizers.l2(l=0.5 * (0.0001))
-        input_var = tf.keras.Input(shape=(112, 8 * 8))
-        x_planes = tf.keras.layers.Reshape([112, 8, 8])(input_var)
-        policy, value, moves_left = self.construct_net(x_planes)
+        input_var = tf.keras.Input(shape=(112, 8, 8))
+        policy, value, moves_left = self.construct_net(input_var)
         if self.moves_left:
             outputs = [policy, value, moves_left]
         else: