5a1fd74ba177c19b89cd183cee95fd147a615b20
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-01-31 19:15:35 +0800

    Refactor net-to-model conversion to improve modularity
    Consolidate net-to-model conversion in a separate function to enhance
    modularity and reduce repeated code.

--- tf/net_to_coreml.py
+++ tf/net_to_coreml.py
@@ -1,46 +1,12 @@
 #!/usr/bin/env python3
-import argparse
 import os
-import yaml
-import tfprocess
+from net_to_model import convert
 import coremltools as ct
 
 if __name__ == "__main__":
     ##############
     # NET TO MODEL
-    argparser = argparse.ArgumentParser(description="Convert net to coreml.")
-    argparser.add_argument(
-        "net", type=str, help="Net file to be converted to a model checkpoint."
-    )
-    argparser.add_argument(
-        "--start", type=int, default=0, help="Offset to set global_step to."
-    )
-    argparser.add_argument(
-        "--cfg",
-        type=argparse.FileType("r"),
-        help="yaml configuration with training parameters",
-    )
-    argparser.add_argument(
-        "-e",
-        "--ignore-errors",
-        action="store_true",
-        help="Ignore missing and wrong sized values.",
-    )
-    args = argparser.parse_args()
-    cfg = yaml.safe_load(args.cfg.read())
-    print(yaml.dump(cfg, default_flow_style=False))
-    START_FROM = args.start
-
-    tfp = tfprocess.TFProcess(cfg)
-    tfp.init_net(include_attn_wts_output=False)
-    tfp.replace_weights(args.net, args.ignore_errors)
-    tfp.global_step.assign(START_FROM)
-
-    root_dir = os.path.join(cfg["training"]["path"], cfg["name"])
-    if not os.path.exists(root_dir):
-        os.makedirs(root_dir)
-    tfp.manager.save(checkpoint_number=START_FROM)
-    print("Wrote model to {}".format(tfp.manager.latest_checkpoint))
+    args, root_dir, tfp = convert(include_attn_wts_output=False)
 
     #################
     # MODEL TO COREML
--- tf/net_to_model.py
+++ tf/net_to_model.py
@@ -4,33 +4,38 @@ import os
 import yaml
 import tfprocess
 
-argparser = argparse.ArgumentParser(description='Convert net to model.')
-argparser.add_argument('net',
-                       type=str,
-                       help='Net file to be converted to a model checkpoint.')
-argparser.add_argument('--start',
-                       type=int,
-                       default=0,
-                       help='Offset to set global_step to.')
-argparser.add_argument('--cfg',
-                       type=argparse.FileType('r'),
-                       help='yaml configuration with training parameters')
-argparser.add_argument('-e',
-                       '--ignore-errors',
-                       action='store_true',
-                       help='Ignore missing and wrong sized values.')
-args = argparser.parse_args()
-cfg = yaml.safe_load(args.cfg.read())
-print(yaml.dump(cfg, default_flow_style=False))
-START_FROM = args.start
+def convert(include_attn_wts_output=True):
+    argparser = argparse.ArgumentParser(description='Convert net to model.')
+    argparser.add_argument('net',
+                        type=str,
+                        help='Net file to be converted to a model checkpoint.')
+    argparser.add_argument('--start',
+                        type=int,
+                        default=0,
+                        help='Offset to set global_step to.')
+    argparser.add_argument('--cfg',
+                        type=argparse.FileType('r'),
+                        help='yaml configuration with training parameters')
+    argparser.add_argument('-e',
+                        '--ignore-errors',
+                        action='store_true',
+                        help='Ignore missing and wrong sized values.')
+    args = argparser.parse_args()
+    cfg = yaml.safe_load(args.cfg.read())
+    print(yaml.dump(cfg, default_flow_style=False))
+    START_FROM = args.start
 
-tfp = tfprocess.TFProcess(cfg)
-tfp.init_net()
-tfp.replace_weights(args.net, args.ignore_errors)
-tfp.global_step.assign(START_FROM)
+    tfp = tfprocess.TFProcess(cfg)
+    tfp.init_net(include_attn_wts_output)
+    tfp.replace_weights(args.net, args.ignore_errors)
+    tfp.global_step.assign(START_FROM)
 
-root_dir = os.path.join(cfg['training']['path'], cfg['name'])
-if not os.path.exists(root_dir):
-    os.makedirs(root_dir)
-tfp.manager.save(checkpoint_number=START_FROM)
-print("Wrote model to {}".format(tfp.manager.latest_checkpoint))
+    root_dir = os.path.join(cfg['training']['path'], cfg['name'])
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+    tfp.manager.save(checkpoint_number=START_FROM)
+    print("Wrote model to {}".format(tfp.manager.latest_checkpoint))
+    return args, root_dir, tfp
+
+if __name__ == "__main__":
+    convert()
